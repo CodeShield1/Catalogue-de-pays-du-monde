a19a3ee5c900f6902a690a04460eb4119ff2e53d
ibrahim_medkour <medkourbrahim08@gmial.com>
2025-11-20 11:14:05 +0000

    code js

---
+++ protoype/script.js
@@ -0,0 +1,47 @@
+
+let container = document.getElementById('container');
+let select=document.getElementById('continents');
+
+let allCountries = [];
+fetch('https://countries-api-hsak.onrender.com/api/countries')
+    .then(res=>res.json())
+    .then(data=>{
+       allCountries=data;
+       displaycountries(allCountries);
+    })
+    .catch(error=>{
+      console.error(error)
+    })
+
+   
+      function displaycountries(list){
+        container.innerHTML=``;
+        list.forEach(element => {
+          let card =document.createElement('div')
+          card.classList.add('card')
+          card.innerHTML=`
+          <img src ="${element.flag}">
+          <h3>${element.name}</h3>
+          `;
+          container.appendChild(card)
+        })
+      }
+
+
+      select.addEventListener('change',function(){
+        let continents = select.value
+
+        if(continents==="all"){
+          displaycountries(allCountries);
+          return;
+        }
+
+        const filtercountry = allCountries.filter(c=>c.continent===continents);
+        displaycountries(filtercountry)
+      });
+
+      
+
+      
+    
+     
